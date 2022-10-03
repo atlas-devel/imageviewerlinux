3ea134d0ecaf0dec9893aaf20e0d8b2770e33adb
Emma <89472272+emma-devel@users.noreply.github.com>
2022-10-03 14:47:33 +0200

    Quality Of Life Improvements
    - Add a Makefile.
    - Add 60 FPS limit to prevent lag when dragging around window. 
    - Now resize image to the biggest resolution that can fit the monitor, rather than just halving it.

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	$(CXX) main.cc -lSDL2 -lSDL2_image -lX11 -o imageviewerlinux	
--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+// #include "gcd.h"
 //#include <Windows.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -34,7 +35,7 @@ int main(int argc, char* argv[]) {
 	// int screeny = GetSystemMetrics(SM_CYFULLSCREEN);
 	int newx = -1;
 	int newy = -1;
-	int downscaled = 0;
+	// int downscaled = 0;
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "best");
 	SDL_Window* window = nullptr;
@@ -46,21 +47,31 @@ int main(int argc, char* argv[]) {
 	if (imageSurface->w > screenx || imageSurface->h > screeny) {
 		newx = imageSurface->w;
 		newy = imageSurface->h;
+		int n1, n2, i, gcd;
+		n1 = newx;
+		n2 = newy;
+		for(i=1;i <= std::min(n1,n2); ++i) {
+			if(n1%i==0&&n2%i==0){
+				gcd = i;
+			}
+		}	
+		std::cout << "GCD: " << gcd << std::endl;
+		int aspectx = newx / gcd;
+		std::cout << "ASPECTX: " << aspectx << std::endl;
+		int aspecty = newy / gcd;
+		std::cout << "ASPECTY: " << aspecty << std::endl;
 		while (newx > screenx || newy > screeny) {
-			if (downscaled < 30) {
+			if(gcd != 1) {	
+				newx = newx - aspectx;
+				newy = newy - aspecty;
+				// downscaled++;
+				// std::cout << "Resolution too big. Downscaling." << std::endl;
+			} else {
 				newx = round(newx / 2);
 				newy = round(newy / 2);
-				downscaled++;
-				std::cout << "Resolution too big. Downscaling." << std::endl;
+				// downscaled++;
+				// std::cout << "Resolution too big. Downscaling." << std::endl;
 			}
-			else {
-				break;
-			}
-		
-		}
-		if (downscaled >= 30) {
-			std::cout << "Resolution way too big. Quitting." << std::endl;
-			return ERROR_RESOLUTION_TOO_BIG;
 		}
 
 		window = SDL_CreateWindow("Image", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, newx, newy, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
@@ -80,7 +91,11 @@ int main(int argc, char* argv[]) {
 	SDL_FreeSurface(imageSurface);
 	SDL_RenderCopy(renderer, imageTex, NULL, NULL);
 	SDL_RenderPresent(renderer);
+	int fps = 60;
+	int desiredDelta = 1000/fps;
+
 	while (!quit) {
+		int startLoop = SDL_GetTicks();
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
@@ -98,7 +113,10 @@ int main(int argc, char* argv[]) {
 			
 			
 		}
-		
+		int delta = SDL_GetTicks() - startLoop;
+		if(delta < desiredDelta) {
+			SDL_Delay(desiredDelta - delta);
+		}
 	}
 	SDL_DestroyTexture(imageTex);
 	SDL_DestroyRenderer(renderer);
